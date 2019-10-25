b3536d8aa09c7ffdef827da1e4f02db3ce68168c
admin <admin@admins-MacBook-Pro-2.local>
2019-10-25 13:32:46 +0530

    gitcacheclr

--- app/src/main/java/com/loconav/lookup/application/BaseView.java
+++ app/src/main/java/com/loconav/lookup/application/BaseView.java
@@ -4,7 +4,7 @@ package com.loconav.lookup.application;
  * Created by prateek on 13/06/18.
  */
 
-public interface  BaseView {
+public interface BaseView {
     void showToast(int resId);
     void showToast(String message);
 }
