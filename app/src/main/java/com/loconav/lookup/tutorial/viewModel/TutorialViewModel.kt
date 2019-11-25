6609a384aae0c880b9f2cf97b4f07e9baf9be8c9
SahilBansal <sahil.bansal@loconav.com>
2019-11-25 13:18:32 +0530

    tutorial data removed

--- app/src/main/java/com/loconav/lookup/tutorial/viewModel/TutorialViewModel.kt
+++ app/src/main/java/com/loconav/lookup/tutorial/viewModel/TutorialViewModel.kt
@@ -2,7 +2,6 @@ package com.loconav.lookup.tutorial.viewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.loconav.lookup.tutorial.model.dataClass.TutorialData
 import com.loconav.lookup.tutorial.model.dataClass.TutorialObject
 import com.loconav.lookup.tutorial.model.repository.TutorialApiService
 import com.loconav.lookup.utils.DataWrapper
