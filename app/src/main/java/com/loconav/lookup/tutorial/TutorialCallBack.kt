a029710d47a09e6f13fed596d5d1cb5ffad5541c
SahilBansal <sahil.bansal@loconav.com>
2019-11-26 16:18:22 +0530

    self_pr

--- app/src/main/java/com/loconav/lookup/tutorial/TutorialCallBack.kt
+++
@@ -1,9 +0,0 @@
-package com.loconav.lookup.tutorial
-
-import android.view.View
-
-interface TutorialCallBack {
-     fun onEventDone(`object`: Any)
-
-    fun onExpendEvent(`object`: Any,view: View)
-}
