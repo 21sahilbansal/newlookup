b2e26c56bc64d157381c17fde5bd262016971f29
SahilBansal <sahil.bansal@loconav.com>
2019-11-29 17:10:32 +0530

    datbinding

--- app/src/main/java/com/loconav/lookup/ignitontest/model/dataClass/IgnitionTests.kt
+++ app/src/main/java/com/loconav/lookup/ignitontest/model/dataClass/IgnitionTests.kt
@@ -1,7 +1,33 @@
 package com.loconav.lookup.ignitontest.model.dataClass
 
-data class IgnitionTests(
-    val TEST_1: TEST1,
-    val TEST_2: TEST2,
-    val TEST_3: TEST3
-)
+import com.google.gson.annotations.SerializedName
+
+class IgnitionTests {
+
+    @SerializedName("TEST_1")
+    var TEST_1: TEST1? = null
+
+    @SerializedName("TEST_2")
+    var TEST_2: TEST2? = null
+
+    @SerializedName("TEST_3")
+    var TEST_3: TEST3? = null
+
+
+    fun getTestAtPosition(testPosition: Int): Any? {
+        when (testPosition) {
+            1 -> {
+                return TEST_1 as Any
+            }
+            2 -> {
+                return TEST_2 as Any
+            }
+            3 -> {
+                return TEST_3 as Any
+            }
+            else -> return null
+        }
+
+    }
+
+}
