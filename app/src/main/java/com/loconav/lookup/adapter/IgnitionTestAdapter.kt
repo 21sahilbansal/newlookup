d5e66ed000da9277b2da21adda17cc0060f9661b
SahilBansal <sahil.bansal@loconav.com>
2019-11-29 13:41:44 +0530

    adapter created

--- app/src/main/java/com/loconav/lookup/adapter/IgnitionTestAdapter.kt
+++ app/src/main/java/com/loconav/lookup/adapter/IgnitionTestAdapter.kt
@@ -1,4 +1,29 @@
 package com.loconav.lookup.adapter
 
-class IgnitionTestAdapter {
+import android.view.View
+import android.view.ViewGroup
+import com.loconav.lookup.R
+import com.loconav.lookup.base.BaseAdapter
+import com.loconav.lookup.ignitontest.model.dataClass.IgnitionTestData
+
+class IgnitionTestAdapter(var ignitionTestData: IgnitionTestData) : BaseAdapter() {
+    override fun getItemCount(): Int {
+        return ignitionTestData.no_of_test_headers
+    }
+
+    override fun getDataAtPosition(position: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getLayoutIdForType(viewType: Int): Int {
+        return  R.layout.item_ignition_test
+    }
+
+    override fun onItemClick(`object`: Any?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun editHeightWidthItem(view: View?, parent: ViewGroup?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
--- app/src/main/java/com/loconav/lookup/ignitontest/view/IgnitionTestFragment.kt
+++ app/src/main/java/com/loconav/lookup/ignitontest/view/IgnitionTestFragment.kt
@@ -1,29 +1,71 @@
 package com.loconav.lookup.ignitontest.view
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.loconav.lookup.R
 import com.loconav.lookup.adapter.IgnitionTestAdapter
 import com.loconav.lookup.application.LookUpApplication
 import com.loconav.lookup.base.BaseFragment
 import com.loconav.lookup.databinding.FragmentIgnitionTestBinding
+import com.loconav.lookup.ignitontest.model.dataClass.IgnitionTestData
 import com.loconav.lookup.ignitontest.viewModel.IgnitionTestViewModel
 
 class IgnitionTestFragment : BaseFragment() {
     private lateinit var ignitionTestBinding: FragmentIgnitionTestBinding
     private lateinit var ignitionTestAdapter: IgnitionTestAdapter
     private lateinit var ignitionTestViewModel: IgnitionTestViewModel
-    private lateinit var ignitionTestReyclerView : RecyclerView
+    private lateinit var ignitionTestReyclerView: RecyclerView
+    private lateinit var alertDialog: Dialog
+    private lateinit var deviceId: String
+    private lateinit var testStartTime: String
 
     override fun setViewId(): Int {
         return R.layout.fragment_ignition_test
     }
 
     override fun onFragmentCreated() {
-        ignitionTestViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(LookUpApplication()).create(IgnitionTestViewModel ::class.java)
+        deviceId = arguments?.get(getString(R.string.deviceDetail_deviceid)) as String
+        ignitionTestViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(LookUpApplication()).create(IgnitionTestViewModel::class.java)
+        showAlertDialog()
+    }
 
+    private fun showAlertDialog() {
+        alertDialog = AlertDialog.Builder(context)
+                .setTitle("Ignition Test")
+                .setMessage("Press ok to start the ignition test")
+                .setPositiveButton("Yes") { dialogInterface, i ->
+                    getIgnitionData()
+
+                }
+                .setNegativeButton("No") { dialogIntertface, j ->
+
+                }
+                .create()
+        alertDialog.setCanceledOnTouchOutside(false)
+        alertDialog.show()
+    }
+
+    private fun getIgnitionData() {
+        testStartTime = (System.currentTimeMillis() / 1000).toString()
+        ignitionTestViewModel.getIgnitionTestData(deviceId, testStartTime)?.observe(this, Observer {it.data?.let {
+            setRecyclerView(it)
+        }
+
+        })
+    }
+
+    private fun setRecyclerView(ignitionTestData: IgnitionTestData) {
+        var linearLayoutManager = LinearLayoutManager(context)
+        ignitionTestReyclerView = ignitionTestBinding.ignitionTestRv
+        ignitionTestReyclerView.layoutManager = linearLayoutManager
+        ignitionTestAdapter = IgnitionTestAdapter(ignitionTestData)
+        ignitionTestReyclerView.adapter = ignitionTestAdapter
     }
 
     override fun bindView(view: View?) {
@@ -36,3 +78,6 @@ class IgnitionTestFragment : BaseFragment() {
     override fun getComponentFactory() {
     }
 }
+
+
+
