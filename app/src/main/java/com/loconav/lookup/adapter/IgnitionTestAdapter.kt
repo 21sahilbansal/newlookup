8d8731ce8674661651ac718f456785fa635245a6
SahilBansal <sahil.bansal@loconav.com>
2019-12-02 09:32:31 +0530

    adapter_created

--- app/src/main/java/com/loconav/lookup/adapter/IgnitionTestAdapter.kt
+++ app/src/main/java/com/loconav/lookup/adapter/IgnitionTestAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.loconav.lookup.R
 import com.loconav.lookup.databinding.ItemIgnitionTestBinding
@@ -13,50 +14,48 @@ import com.loconav.lookup.ignitontest.model.dataClass.TestNo
 
 class IgnitionTestAdapter(var ignitionTestData: IgnitionTestData) : RecyclerView.Adapter<IgnitionTestAdapter.IgnitionTestViewHolder>() {
     class IgnitionTestViewHolder : RecyclerView.ViewHolder {
-        var testTitle: TextView
-        var batteryStatus: TextView
-        var ignitionStatus: TextView
-        var overAllTestStatus: TextView
-
+        val testTitle: TextView
+        val batteryStatus: TextView
+        val ignitionStatus: TextView
+        val overAllTestStatus: TextView
         constructor(itemIignitionTestBinding: ItemIgnitionTestBinding) : super(itemIignitionTestBinding.root) {
-
             testTitle = itemIignitionTestBinding.testtitleTv
             batteryStatus = itemIignitionTestBinding.batteryStatusEv
             ignitionStatus = itemIignitionTestBinding.ignitionStatusEv
             overAllTestStatus = itemIignitionTestBinding.testStatusEv
         }
-
-
     }
-
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IgnitionTestViewHolder {
         val itemView: View = LayoutInflater.from(parent.context).inflate(R.layout.item_ignition_test, parent, false)
-        var itemIgnitionTestBinding: ItemIgnitionTestBinding = DataBindingUtil.bind(itemView)!!
-          return IgnitionTestViewHolder(itemIgnitionTestBinding)
+        var itemIgnitionTestBinding: ViewDataBinding = DataBindingUtil.bind(itemView)!!
+        return IgnitionTestViewHolder(itemIgnitionTestBinding as ItemIgnitionTestBinding)
     }
 
     override fun getItemCount(): Int {
         return ignitionTestData.let { it.noOfTests!! }
-
     }
 
     override fun onBindViewHolder(holder: IgnitionTestViewHolder, position: Int) {
-       bindDataToView(holder,position)
+        bindDataToView(holder, position)
     }
-
     private fun bindDataToView(holder: IgnitionTestAdapter.IgnitionTestViewHolder, position: Int) {
-       var testNo : TestNo ?=null
-        when(position){
-          1->testNo = ignitionTestData.ignitionTests?.test1
-          2->testNo = ignitionTestData.ignitionTests?.test2
-          3->testNo = ignitionTestData.ignitionTests?.test3
-      }
+        var testNo: TestNo? = null
+        when (position) {
+            0 -> testNo = ignitionTestData.ignitionTests?.test1
+            1 -> testNo = ignitionTestData.ignitionTests?.test2
+            2 -> testNo = ignitionTestData.ignitionTests?.test3
+        }
         holder.testTitle.text = testNo?.name
         holder.batteryStatus.text = testNo?.battery
         holder.ignitionStatus.text = testNo?.ignition
-        holder.overAllTestStatus.text = testNo?.status as String
+        holder.overAllTestStatus.text = getTestStatus(testNo?.status)
     }
 
-
+    private fun getTestStatus(status: Int?): CharSequence? {
+        if (status == 1) {
+            return "Status Ok"
+        } else {
+            return "Status Not Ok"
+        }
+    }
 }
